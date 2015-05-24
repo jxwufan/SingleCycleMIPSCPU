7fdf53fca2aed4b35dbc6f716f88662ca9da350c
root <root@localhost.localdomain>
2015-05-24 20:39:22 +0800

    add some instructions

--- sccu_dataflow.v
+++ sccu_dataflow.v
@@ -37,6 +37,7 @@ module sccu_dataflow(op, func, z, wmem, wreg, regrt, m2reg, aluc, shift, aluimm,
 	wire i_jr 	= r_type &~func[5] &~func[4] & func[3] &~func[2] &~func[1] &~func[0];
 	wire i_nor 	= r_type & func[5] &~func[4] &~func[3] & func[2] & func[1] & func[0];
 	wire i_slt 	= r_type & func[5] &~func[4] & func[3] &~func[2] & func[1] &~func[0];
+	wire i_jalr = r_type &~func[5] &~func[4] & func[3] &~func[2] &~func[1] & func[0];
 	wire i_addi	= ~op[5]&~op[4]& op[3]&~op[2]&~op[1]&~op[0];
 	wire i_andi	= ~op[5]&~op[4]& op[3]& op[2]&~op[1]&~op[0];
 	wire i_ori	= ~op[5]&~op[4]& op[3]& op[2]&~op[1]& op[0];
@@ -48,19 +49,20 @@ module sccu_dataflow(op, func, z, wmem, wreg, regrt, m2reg, aluc, shift, aluimm,
 	wire i_lui	= ~op[5]&~op[4]& op[3]& op[2]& op[1]& op[0];
 	wire i_j		= ~op[5]&~op[4]&~op[3]&~op[2]& op[1]&~op[0];
 	wire i_jal	= ~op[5]&~op[4]&~op[3]&~op[2]& op[1]& op[0];
+	wire i_slti = ~op[5]&~op[4]& op[3]&~op[2]& op[1]&~op[0];
 	
-	assign wreg 	= i_add | i_sub | i_and | i_or | i_xor | i_sll | i_srl | i_sra | i_addi | i_andi | i_ori | i_xori | i_lw | i_lui | i_jal | i_slt | i_nor;
-	assign regrt	= i_addi | i_andi | i_ori | i_xori | i_lw | i_lui;
-	assign jal 		= i_jal;
+	assign wreg 	= i_add | i_sub | i_and | i_or | i_xor | i_sll | i_srl | i_sra | i_addi | i_andi | i_ori | i_xori | i_lw | i_lui | i_jal | i_slt | i_nor | i_slti | i_jalr;
+	assign regrt	= i_addi | i_andi | i_ori | i_xori | i_lw | i_lui | i_slti;
+	assign jal 		= i_jal | i_jalr;
 	assign m2reg 	= i_lw;
 	assign shift	= i_sll | i_srl | i_sra;
-	assign aluimm	= i_addi | i_andi | i_ori | i_xori | i_lw | i_lui;
-	assign sext 	= i_addi | i_lw | i_sw | i_beq | i_bne;
-	assign aluc[3] = i_sra | i_slt | i_nor;
+	assign aluimm	= i_addi | i_andi | i_ori | i_xori | i_lw | i_lui | i_sw | i_slti;
+	assign sext 	= i_addi | i_lw | i_sw | i_beq | i_bne | i_slti;
+	assign aluc[3] = i_sra | i_slt | i_nor | i_slti;
 	assign aluc[2] = i_sub | i_or | i_srl | i_sra | i_ori | i_lui | i_nor;
 	assign aluc[1] = i_xor | i_sll | i_srl | i_sra | i_xori | i_beq | i_bne | i_lui;
 	assign aluc[0] = i_and | i_or | i_sll | i_srl | i_sra | i_andi | i_ori | i_nor;
 	assign wmem 	= i_sw;
-	assign pcsource[1] = i_jr | i_j | i_jal;
+	assign pcsource[1] = i_jr | i_j | i_jal | i_jalr;
 	assign pcsource[0] = i_beq&z | i_bne&~z |i_j |i_jal;
 endmodule
